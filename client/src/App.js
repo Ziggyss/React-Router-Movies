34fb7295b001b9a72b94bb67828713ea3062c649
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-09-16 14:27:36 +0100

    begin set up of Router on app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,20 @@
-import React, { useState } from 'react';
-
-import SavedList from './Movies/SavedList';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import MovieList from '../src/Movies/MovieList';
+import SavedList from "./Movies/SavedList";
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([]);
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    setSavedList([...savedList, movie]);
   };
 
   return (
-    <div>
-      <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
-    </div>
+    <Router>
+      <Route exact path="/" component={MovieList}></Route>
+      <Route></Route>
+    </Router>
   );
 };
 
