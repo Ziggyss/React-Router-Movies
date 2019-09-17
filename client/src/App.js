064b91632a74d241d628d8e072a18cb93dc6b147
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-09-17 14:31:14 +0100

    fix bugs and get second project working

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import MovieList from '../src/Movies/MovieList';
 import SavedList from "./Movies/SavedList";
+import Movie from "./Movies/Movie";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -12,8 +13,14 @@ const App = () => {
 
   return (
     <Router>
+      <div>
+        <SavedList list={savedList} />
+      </div>
       <Route exact path="/" component={MovieList}></Route>
-      <Route></Route>
+      <Route 
+      path="/movies/:id" 
+      render={props => <Movie {...props} addToSavedList={addToSavedList}/> }>
+      </Route>
     </Router>
   );
 };
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,12 +2,11 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Movie = (props) => {
-  const [movie, setMovie] = useState({});
+  const [movie, setMovie] = useState();
  
   useEffect(() => {
-    const id = 1;
-    // change ^^^ that line and grab the id from the URL
-    // You will NEED to add a dependency array to this effect hook
+    const id = props.match.params.id;
+    console.log('Id console log ' + id);
 
        axios
         .get(`http://localhost:5000/api/movies/${id}`)
@@ -18,7 +17,7 @@ const Movie = (props) => {
           console.error(error);
         });
 
-  },[]);
+  },[props.match.params.id]);
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
