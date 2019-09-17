f877ac5a12159b729985e3c28afd1a8ca7d14974
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-09-17 14:44:26 +0100

    add functionality to home button and save movie button

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -19,11 +19,11 @@ const Movie = (props) => {
 
   },[props.match.params.id]);
   
-  // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  
+  const saveMovie = () => {
+  const addToSavedList = props.addToSavedList;
+  addToSavedList(movie)
+  }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -48,7 +48,7 @@ const Movie = (props) => {
           </div>
         ))}
       </div>
-      <div className="save-button">Save</div>
+      <div onClick={saveMovie} className="save-button">Save</div>
     </div>
   );
 }
